d3b5353a704225eb9fac6763a77f22c8c977fcdd
Gantzyo <15526377+Gantzyo@users.noreply.github.com>
2019-03-02 12:25:06 +0100

    Ya no se corta el thread aunque se cierre la conexión por parte del cliente

--- src/update.php
+++ src/update.php
@@ -7,6 +7,8 @@ use RssGenerator\Service\CookieService as CookieService;
 use RssGenerator\Service\SiteService as SiteService;
 use RssGenerator\Service\LastSiteUpdateService as LastSiteUpdateService;
 
+ignore_user_abort(true);
+
 // Uncomment to unlimit var_dump
 ini_set('xdebug.var_display_max_depth', '-1');
 ini_set('xdebug.var_display_max_children', '-1');
@@ -33,4 +35,5 @@ foreach ($sites as $site) {
     }
 }
 
-var_dump(ConnectionFactory::getFactory()->getConnection()->log());
+echo "OK";
+?>
