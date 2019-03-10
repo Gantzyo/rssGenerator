761c25ebdfac6cf83d627d6cd4ca8e1a692ee5fd
Gantzyo <15526377+Gantzyo@users.noreply.github.com>
2019-03-11 00:44:57 +0100

    Ahora el rss no intenta mostrar los nuevos sitios que no han sido actualizados aún.

--- src/repository/FeedHasSiteRepository.php
+++ src/repository/FeedHasSiteRepository.php
@@ -19,17 +19,25 @@ class FeedHasSiteRepository
 
     public static function findSitesByFeedOrderByUpdate(int $id): array
     {
+        /*
+        SELECT s.* FROM rssgenerator_feed_has_site fhs
+        LEFT JOIN rssgenerator_site s ON fhs.Site_id=s.id
+        RIGHT JOIN rssgenerator_last_site_update lsu ON lsu.Site_id = s.id
+        WHERE fhs.Feed_id = 1
+        AND fhs.enabled = TRUE
+        ORDER BY lsu.updateTS DESC, s.id ASC;
+         */
         $db = ConnectionFactory::getFactory()->getConnection();
-        return $db->select("feed_has_site",[
+        return $db->select("feed_has_site", [
             "[>]site" => ["feed_has_site.Site_id" => "id"],
-            "[>]last_site_update" => ["site.id" => "Site_id"],
+            "[<]last_site_update" => ["site.id" => "Site_id"],
         ], RepositoryUtils::getDomainColumns(Site::class, "site"), [
             "feed_has_site.Feed_id" => $id,
             "feed_has_site.enabled" => true,
             "ORDER" => [
                 "last_site_update.updateTS" => "DESC",
-                "site.id" => "ASC"
-            ]
+                "site.id" => "ASC",
+            ],
         ]);
     }
 }
