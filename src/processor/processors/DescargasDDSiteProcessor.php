b5a8688bea41b0669680ed299399794720f28512
Gantzyo <15526377+Gantzyo@users.noreply.github.com>
2023-11-20 22:49:58 +0100

    Sacar el titulo sin usar QueryList

--- src/processor/processors/DescargasDDSiteProcessor.php
+++ src/processor/processors/DescargasDDSiteProcessor.php
@@ -2,7 +2,6 @@
 namespace RssGenerator\Processor\Processors;
 
 use Requests;
-use QL\QueryList as QueryList;
 use RssGenerator\Domain\Cookie as Cookie;
 use RssGenerator\Domain\Site as Site;
 use RssGenerator\Processor\ISiteProcessor as ISiteProcessor;
@@ -18,27 +17,32 @@ class DescargasDDSiteProcessor implements ISiteProcessor
     public function getSiteUpdate(Site $site, $cookies): ?string
     {
         echo "Recuperando cookies como string<br/>";
-		$cookiesString = SiteProcessorUtils::getCookiesAsString($cookies);
+        $cookiesString = SiteProcessorUtils::getCookiesAsString($cookies);
         
-		echo "Steando cookies como headers<br/>";
+        echo "Steando cookies como headers<br/>";
         $headers = [];
         if ($cookiesString != null) {
             $headers["Cookie"] = $cookiesString;
         }
 
-		echo "Haciendo GET a la URL del sitio<br/>";
+        echo "Haciendo GET a la URL del sitio<br/>";
         $result = Requests::get($site->url, $headers);
 
-		echo "Limpiando el body<br/>";
+        // OLD - Usando QueryList
+        // echo "Limpiando el body<br/>";
         // Quitamos las tags de scripts para qutiarnos bastantes warnings al porcesar el HTML (Siguen saltando alguno que otro)
-        $cleanBody = SiteProcessorUtils::getHtmlWithoutScripts($result->body);
+        // $cleanBody = SiteProcessorUtils::getHtmlWithoutScripts($result->body);
 
-		echo "Recuperando el valor buscado<br/>";
-        // Esto produce alguno warnings pero funciona
+        // echo "Recuperando el valor buscado<br/>";
         // if($site->idWeb === "title") // Añadir si en el futuro hay que buscar otros campos según el tema a consultar
-        $value = QueryList::setHtml($cleanBody)->find(".threadtitle a")->text();
+        // $value = QueryList::setHtml($cleanBody)->find(".threadtitle a")->text();
 
-		echo "Valor recuperado:".$value."<br/>";
+        echo "Recuperando el valor buscado<br/>";
+        preg_match("/<title>(.*)<\/title>/i", $result->body, $matches);
+
+        $value = $matches[1];
+
+        echo "Valor recuperado:".$value."<br/>";
         return $value;
     }
 }
